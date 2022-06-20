bfc4ab2e9a1f848815785c4c273287bf41e1b78d
Sebastian Villegas <juan.villega@mercadolibre.com.co>
2022-06-19 19:33:49 -0500

    Add metatags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,39 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <title>{'TalksUp'}</title>
+        <meta name='author' content='Sebasvil20' />
+        <meta name='description' content={`TalksUp - Find your next podcast`} />
+        <meta name='keywords' content={`podcasts, recommendations, app`} />
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+        <meta
+          property='og:title'
+          content={"Talk's up find your next podcast"}
+        />
+        <meta property='og:url' content='https://web.talksup.app/' />
+        <meta
+          property='og:image'
+          content='https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/724ecd00-de05-11ec-b35a-e62effe0ba4e.png'
+        />
+        <meta
+          name='twitter:title'
+          content="Talk's Up - Find your next podcast"
+        />
+        <meta
+          name='twitter:description'
+          content='See thru the biggest podcast community'
+        />
+        <meta
+          name='twitter:image'
+          content='https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/724ecd00-de05-11ec-b35a-e62effe0ba4e.png'
+        />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
       <header className='bg-white'>
         <div className='max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8'>
           <div className='flex items-center justify-between h-16'>
